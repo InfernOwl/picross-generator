dcad8d167b1d7d50da1540ebb418529b7db0d211
InfernOwl <infernowl@Blanket-1.5>
2020-09-02 08:38:31 -0400

    Adding x-marking function on right click, as well as more comments for code clarity

--- src/Square.js
+++ src/Square.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import emptySquare from './assets/iconmonstr-square-4.svg';
 import filledSquare from './assets/iconmonstr-square-1.svg';
+import filledX from './assets/iconmonstr-x-mark-1.svg';
 
 class Square extends React.Component {
 
+    imageSelect(image) {
+        var newImage;
+
+        switch (image) {
+            case "empty": newImage = emptySquare;
+                break;
+            case "filled": newImage = filledSquare;
+                break;
+            case "X": newImage = filledX;
+                break;
+            default:
+                break;
+        }
+
+        return newImage;
+    }
+
     render() {
         return(
-            <img src={this.props.image === 'empty' ? emptySquare : filledSquare} alt='' className="Test" onClick = {(e) => this.props.onClick(e)}></img>
+            <img src={this.imageSelect(this.props.image)} alt='' className="Test" onClick={(e) => this.props.onClick(e)} onContextMenu={(e) => this.props.onContextMenu(e)}></img>
         );
         }
 }
