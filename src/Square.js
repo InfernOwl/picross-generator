1cca1ce41986519fd76ffabbbdb245ed9d672f6f
ish <87158272+ishWalker@users.noreply.github.com>
2023-10-31 13:49:22 -0400

    Prettier update

--- src/Square.js
+++ src/Square.js
@@ -1,33 +1,45 @@
-import React from 'react';
-import emptySquare from './assets/iconmonstr-square-4.svg';
-import filledSquare from './assets/iconmonstr-square-1.svg';
-import filledX from './assets/iconmonstr-x-mark-1.svg';
+import React from "react";
+import emptySquare from "./assets/iconmonstr-square-4.svg";
+import filledSquare from "./assets/iconmonstr-square-1.svg";
+import filledX from "./assets/iconmonstr-x-mark-1.svg";
 
 class Square extends React.Component {
+  imageSelect(image) {
+    var newImage;
 
-    imageSelect(image) {
-        var newImage;
-
-        switch (image) {
-            case "empty": newImage = emptySquare;
-                break;
-            case "filled": newImage = filledSquare;
-                break;
-            case "X": newImage = filledX;
-                break;
-            default:
-                break;
-        }
-
-        return newImage;
+    switch (image) {
+      case "empty":
+        newImage = emptySquare;
+        break;
+      case "filled":
+        newImage = filledSquare;
+        break;
+      case "X":
+        newImage = filledX;
+        break;
+      default:
+        break;
     }
 
-    render() {
-        return(
-            <img draggable="false" src={this.imageSelect(this.props.image)} alt='' className="Test" xpos={this.props.xpos} ypos={this.props.ypos} sqnum={this.props.sqnum} onMouseDown={(e) => this.props.onMouseDown(e)} onContextMenu={(e) => this.props.onContextMenu(e)} onMouseEnter={(e) => this.props.onMouseEnter(e)}></img>
-        );
-        }
-}
+    return newImage;
+  }
 
+  render() {
+    return (
+      <img
+        draggable="false"
+        src={this.imageSelect(this.props.image)}
+        alt=""
+        className="Test"
+        xpos={this.props.xpos}
+        ypos={this.props.ypos}
+        sqnum={this.props.sqnum}
+        onMouseDown={(e) => this.props.onMouseDown(e)}
+        onContextMenu={(e) => this.props.onContextMenu(e)}
+        onMouseEnter={(e) => this.props.onMouseEnter(e)}
+      ></img>
+    );
+  }
+}
 
 export default Square;
