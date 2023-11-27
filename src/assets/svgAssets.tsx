09e0d6ad88ac0a7fde876d8464d2f07968b6e436
ish <87158272+ishWalker@users.noreply.github.com>
2023-11-27 16:19:49 -0500

    Delete svgAssets.tsx

--- src/assets/svgAssets.tsx
+++
@@ -1,3 +0,0 @@
-export const EMPTY_SQUARE = "../assets/iconmonstr-square-4.svg";
-export const FILLED_SQUARE = "../assets/iconmonstr-square-1.svg";
-export const FILLED_X = "../assets/iconmonstr-x-mark-1.svg";
